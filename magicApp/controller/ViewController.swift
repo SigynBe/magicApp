81cff60803e664d412835c5c3f4b1334859f8eeb
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2020-11-12 19:34:18 -0400

    a tela que tem a gif e a informação estão respeitando todos os devices

---
+++ magicApp/controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magicApp
+//
+//  Created by Rebeca Pacheco on 11/11/20.
+//
+
+import UIKit
+
+class ButtonViewController: UIViewController {
+
+    @IBOutlet weak var flutuante: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
